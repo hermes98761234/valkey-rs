d2da817148ba94d7853247efb1bcf4ceab25f711
Bobbi-Jo Clark <hermes98761234@tutamail.com>
2026-06-04 18:16:53 +0000

    feat(commands): String command family

--- crates/commands/src/lib.rs
+++ crates/commands/src/lib.rs
@@ -3,6 +3,7 @@ use std::sync::Arc;
 use valkey_proto::RespValue;
 use valkey_storage::Store;
 
+pub mod string;
 pub mod zset;
 
 pub type Db = Arc<Store>;
@@ -16,39 +17,44 @@ pub async fn dispatch(cmd: Vec<Bytes>, store: Db) -> RespValue {
         Err(_) => return RespValue::Error("ERR invalid command name".into()),
     };
     let args = &cmd[1..];
-    let r = match name.as_str() {
+    match name.as_str() {
         "PING" => return RespValue::SimpleString("PONG".into()),
         "QUIT" => return RespValue::SimpleString("OK".into()),
-        "ZADD" => zset::zadd(&store, args),
-        "ZSCORE" => zset::zscore(&store, args),
-        "ZMSCORE" => zset::zmscore(&store, args),
-        "ZRANK" => zset::zrank(&store, args),
-        "ZREVRANK" => zset::zrevrank(&store, args),
-        "ZRANGE" => zset::zrange(&store, args),
-        "ZREVRANGE" => zset::zrevrange(&store, args),
-        "ZRANGEBYSCORE" => zset::zrangebyscore(&store, args),
-        "ZREVRANGEBYSCORE" => zset::zrevrangebyscore(&store, args),
-        "ZRANGEBYLEX" => zset::zrangebylex(&store, args),
-        "ZCOUNT" => zset::zcount(&store, args),
-        "ZLEXCOUNT" => zset::zlexcount(&store, args),
-        "ZREM" => zset::zrem(&store, args),
-        "ZREMRANGEBYRANK" => zset::zremrangebyrank(&store, args),
-        "ZREMRANGEBYSCORE" => zset::zremrangebyscore(&store, args),
-        "ZREMRANGEBYLEX" => zset::zremrangebylex(&store, args),
-        "ZCARD" => zset::zcard(&store, args),
-        "ZINCRBY" => zset::zincrby(&store, args),
-        "ZPOPMIN" => zset::zpopmin(&store, args),
-        "ZPOPMAX" => zset::zpopmax(&store, args),
-        "ZUNIONSTORE" => zset::zunionstore(&store, args),
-        "ZINTERSTORE" => zset::zinterstore(&store, args),
-        "ZDIFFSTORE" => zset::zdiffstore(&store, args),
-        "ZSCAN" => zset::zscan(&store, args),
-        "ZRANDMEMBER" => zset::zrandmember(&store, args),
-        "ZRANGESTORE" => zset::zrangestore(&store, args),
-        _ => return RespValue::Error(format!("ERR unknown command `{}`", name).into()),
-    };
-    match r {
-        Ok(v) => v,
-        Err(e) => RespValue::Error(e),
+        "GET"|"SET"|"DEL"|"GETSET"|"MGET"|"MSET"|"MSETNX"
+        |"INCR"|"DECR"|"INCRBY"|"DECRBY"|"INCRBYFLOAT"|"APPEND"
+        |"STRLEN"|"GETRANGE"|"SETRANGE"|"SETNX"|"SETEX"|"PSETEX"
+        |"GETEX"|"GETDEL" => string::handle(&cmd, &store).await,
+        _ => {
+            let r = match name.as_str() {
+                "ZADD" => zset::zadd(&store, args),
+                "ZSCORE" => zset::zscore(&store, args),
+                "ZMSCORE" => zset::zmscore(&store, args),
+                "ZRANK" => zset::zrank(&store, args),
+                "ZREVRANK" => zset::zrevrank(&store, args),
+                "ZRANGE" => zset::zrange(&store, args),
+                "ZREVRANGE" => zset::zrevrange(&store, args),
+                "ZRANGEBYSCORE" => zset::zrangebyscore(&store, args),
+                "ZREVRANGEBYSCORE" => zset::zrevrangebyscore(&store, args),
+                "ZRANGEBYLEX" => zset::zrangebylex(&store, args),
+                "ZCOUNT" => zset::zcount(&store, args),
+                "ZLEXCOUNT" => zset::zlexcount(&store, args),
+                "ZREM" => zset::zrem(&store, args),
+                "ZREMRANGEBYRANK" => zset::zremrangebyrank(&store, args),
+                "ZREMRANGEBYSCORE" => zset::zremrangebyscore(&store, args),
+                "ZREMRANGEBYLEX" => zset::zremrangebylex(&store, args),
+                "ZCARD" => zset::zcard(&store, args),
+                "ZINCRBY" => zset::zincrby(&store, args),
+                "ZPOPMIN" => zset::zpopmin(&store, args),
+                "ZPOPMAX" => zset::zpopmax(&store, args),
+                "ZUNIONSTORE" => zset::zunionstore(&store, args),
+                "ZINTERSTORE" => zset::zinterstore(&store, args),
+                "ZDIFFSTORE" => zset::zdiffstore(&store, args),
+                "ZSCAN" => zset::zscan(&store, args),
+                "ZRANDMEMBER" => zset::zrandmember(&store, args),
+                "ZRANGESTORE" => zset::zrangestore(&store, args),
+                _ => return RespValue::Error(format!("ERR unknown command `{}`", name).into()),
+            };
+            match r { Ok(v) => v, Err(e) => RespValue::Error(e) }
+        }
     }
 }
